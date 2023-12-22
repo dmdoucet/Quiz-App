90dde3c8c7bac3053f54e24a4b019e59f3adb761
Dylan Doucet <28883259+dmdoucet@users.noreply.github.com>
2023-12-22 13:46:14 -0500

    Update app.js
    added variables for event listeners

--- app.js
+++ app.js
@@ -90,3 +90,7 @@ const questions = [
         ]
     }
 ];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
