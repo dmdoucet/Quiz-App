c9c875b35d5c1f744250b734ad91bada4a103483
Dylan Doucet <28883259+dmdoucet@users.noreply.github.com>
2023-12-22 13:43:43 -0500

    Update app.js
    added questions for the quiz

---
+++ app.js
@@ -0,0 +1,92 @@
+const questions = [
+    {
+        question: "When did the construction of the Great Wall of China begin?",
+        answers: [
+            {text: "7th century BC", correct: true},
+            {text: "1st century", correct: false},
+            {text: "A few years ago", correct: false},
+            {text: "15th century", correct: false}
+        ]
+    },
+    {
+        question: "Who sent Christopher Columbus to explore the New World?",
+        answers: [
+            {text: "King Charles VIII of France", correct: false},
+            {text: "King Henry VII of England", correct: false},
+            {text: "King Ferdinand of Spain", correct: true},
+            {text: "Doge Agostino Barbarigo of Venice", correct: false}
+        ]
+    },
+    {
+        question: "What is considered the largest empire in history?",
+        answers: [
+            {text: "The Roman Empire", correct: false},
+            {text: "The Ottoman Empire", correct: false},
+            {text: "Qing Dynasty", correct: false},
+            {text: "The Mongol Empire", correct: true}
+        ]
+    },
+    {
+        question: "What year did the French Revolution start?",
+        answers: [
+            {text: "1789", correct: true},
+            {text: "1812", correct: false},
+            {text: "1903", correct: false},
+            {text: "1461", correct: false}
+        ]
+    },
+    {
+        question: "What event is commonly believed to have sparked World War I?",
+        answers: [
+            {text: "A game of Rock-Paper-Scissors gone wrong", correct: false},
+            {text: "Marie Antoinetter didn't want to marry King Louis XVI", correct: false},
+            {text: "The assassination of Archduke Franz Ferdinand of Austria", correct: true},
+            {text: "A Serbian soldier was shot after crossing the border into Austria while chasing after his runaway dog.", correct: false}
+        ]
+    },
+    {
+        question: "Who was the last Tsar of Russia?",
+        answers: [
+            {text: "Vlad The Impaler", correct: false},
+            {text: "Borat", correct: false},
+            {text: "Grigori Rasputin", correct: false},
+            {text: "Nicholas II", correct: true}
+        ]
+    },
+    {
+        question: "Where did Albert Einstein live before moving to the United States?",
+        answers: [
+            {text: "Margarittaville", correct: false},
+            {text: "Germany", correct: true},
+            {text: "Austria", correct: false},
+            {text: "Switzerland", correct: false}
+        ]
+    },
+    {
+        question: "Who is commonly referred to as the person who created the first printing press?",
+        answers: [
+            {text: "Johannes Gutenberg", correct: true},
+            {text: "Nikola Tesla", correct: false},
+            {text: "Steve Jobs", correct: false},
+            {text: "Benjamin Franklin", correct: false}
+        ]
+    },
+    {
+        question: "How old was Queen Elizabeth II when she was crowned the Queen of England?",
+        answers: [
+            {text: "40", correct: false},
+            {text: "16", correct: false},
+            {text: "27", correct: true},
+            {text: "21", correct: false}
+        ]
+    },
+    {
+        question: "What was the capital city of the Inca Empire?",
+        answers: [
+            {text: "Yaxchilán", correct: false},
+            {text: "Tenochtitlan", correct: false},
+            {text: "Tijuana", correct: false},
+            {text: "Cusco", correct: true}
+        ]
+    }
+];
